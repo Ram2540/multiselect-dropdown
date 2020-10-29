e06bbc80893464d57f5780b05256c72c1b9487f0
Roman Stoyan <rstoyan@itgua.com>
2020-10-29 11:28:03 +0200

    Updates

---
+++ precache-manifest.3cff7e14d62484fbc6c6fcf4480a47a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457c5e821f6d44c730b7f94a3165ffb6",
+    "url": "/multiselect-dropdown/index.html"
+  },
+  {
+    "revision": "bfbdfbb031853e154b32",
+    "url": "/multiselect-dropdown/static/css/main.25078aee.chunk.css"
+  },
+  {
+    "revision": "907013734eec6b2250be",
+    "url": "/multiselect-dropdown/static/js/2.305eb470.chunk.js"
+  },
+  {
+    "revision": "24bf724123f1af6c67bad1e94211dd14",
+    "url": "/multiselect-dropdown/static/js/2.305eb470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbdfbb031853e154b32",
+    "url": "/multiselect-dropdown/static/js/main.ae836d7b.chunk.js"
+  },
+  {
+    "revision": "9fbe9eb8012c4d7cb832",
+    "url": "/multiselect-dropdown/static/js/runtime-main.4e80ae01.js"
+  }
+]);
